ab347145209d87e9dc93cf8de70c0b86fa7f1acb
WhiteHole <101702658+WhiteHole00@users.noreply.github.com>
2022-11-14 23:22:06 +0900

    Create main.py

---
+++ main.py
@@ -0,0 +1,29 @@
+import discord
+import datetime
+import random
+
+
+token = "봇 토큰"
+
+intents = discord.Intents.default()
+intents.members = True
+client = discord.Client(intents=intents)
+
+years_ = ['2017','2018']
+
+@client.event 
+async def on_member_join(member):
+    date = datetime.datetime.utcfromtimestamp(((int(member.id) >> 22) + 1420070400000) / 1000)
+    random_name = random.randint(1000, 9999)
+    if date.year in years_ or member.name == random_name:
+            await member.send("당신은 경찰 의심 대상으로 확정 되었으므로 서버에서 추방 되었습니다!")
+            await member.kick(reason='경찰 의심 대상')
+    else:
+        await member.send("당신은 경찰 의심 조사에서 피해갔습니다!")
+
+@client.event
+async def on_ready():
+    await client.change_presence(activity=discord.Streaming(name= f"경찰 추방 봇", url="https://www.twitch.tv/whitehole"))
+
+
+client.run(token)        
