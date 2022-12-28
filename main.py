3966a92e8f883fc3d7b4f09306eddcdee6092de9
WhiteHole <101702658+WhiteHole00@users.noreply.github.com>
2022-12-28 15:35:12 +0900

    Update main.py

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ async def on_member_join(member):
     date = datetime.datetime.utcfromtimestamp(((int(member.id) >> 22) + 1420070400000) / 1000)
     random_name = random.randint(1000, 9999)
     if date.year in years_ or member.name == random_name:
-            await member.send("당신은 경찰 의심 대상으로 확정 되었으므로 서버에서 추방 되었습니다!")
+            await member.send("당신은 경찰 의심 대상으로 확정 되었음으로 서버에서 추방 되었습니다!")
             await member.kick(reason='경찰 의심 대상')
     else:
         await member.send("당신은 경찰 의심 조사에서 피해갔습니다!")
